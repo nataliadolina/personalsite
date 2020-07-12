f32a143d07e200be5cd88ece1668a882f23d74f3
Natalia <nat.dol30@mail.ru>
2020-07-12 13:50:25 +0300

    added files

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('', views.index, name=" "),
-               path('add_program/<int:id>', views.add_new_program, name="program")]
+               path('add/<int:id>', views.add_new_program, name="program")]
